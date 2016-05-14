2ee4f2c979f20cc47f9f10c7ad9ca5a2db1ebed9
yyter <yyter@outlook.com>
2016-05-14 17:51:22 +0800

    dispatch WebChromeClient callback to custom client

--- library/src/main/java/com/yyter/web/SafeWebView.java
+++ library/src/main/java/com/yyter/web/SafeWebView.java
@@ -2,9 +2,20 @@ package com.yyter.web;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Message;
 import android.util.AttributeSet;
+import android.view.View;
+import android.webkit.ConsoleMessage;
+import android.webkit.GeolocationPermissions;
 import android.webkit.JsPromptResult;
+import android.webkit.JsResult;
+import android.webkit.PermissionRequest;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
+import android.webkit.WebStorage;
 import android.webkit.WebView;
 
 /**
@@ -62,6 +73,104 @@ public class SafeWebView extends WebView {
 
     private final class SafeWebChromeClient extends WebChromeClient {
         @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+            if(client != null) {
+                client.onProgressChanged(view, newProgress);
+            }
+        }
+
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            super.onReceivedTitle(view, title);
+            if(client != null) {
+                client.onReceivedTitle(view, title);
+            }
+        }
+
+        @Override
+        public void onReceivedIcon(WebView view, Bitmap icon) {
+            super.onReceivedIcon(view, icon);
+            if(client != null) {
+                client.onReceivedIcon(view, icon);
+            }
+        }
+
+        @Override
+        public void onReceivedTouchIconUrl(WebView view, String url, boolean precomposed) {
+            super.onReceivedTouchIconUrl(view, url, precomposed);
+            if(client != null) {
+                client.onReceivedTouchIconUrl(view, url, precomposed);
+            }
+        }
+
+        @Override
+        public void onShowCustomView(View view, CustomViewCallback callback) {
+            super.onShowCustomView(view, callback);
+            if(client != null) {
+                client.onShowCustomView(view, callback);
+            }
+        }
+
+        @Override
+        public void onShowCustomView(View view, int requestedOrientation, CustomViewCallback callback) {
+            super.onShowCustomView(view, requestedOrientation, callback);
+            if(client != null) {
+                if(Build.VERSION.SDK_INT >= 14) {
+                    client.onShowCustomView(view, requestedOrientation, callback);
+                }
+            }
+        }
+
+        @Override
+        public void onHideCustomView() {
+            super.onHideCustomView();
+            if(client != null) {
+                client.onHideCustomView();
+            }
+        }
+
+        @Override
+        public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
+            if(client != null) {
+                return client.onCreateWindow(view, isDialog, isUserGesture, resultMsg);
+            }
+            return super.onCreateWindow(view, isDialog, isUserGesture, resultMsg);
+        }
+
+        @Override
+        public void onRequestFocus(WebView view) {
+            super.onRequestFocus(view);
+            if(client != null) {
+                client.onRequestFocus(view);
+            }
+        }
+
+        @Override
+        public void onCloseWindow(WebView window) {
+            super.onCloseWindow(window);
+            if(client != null) {
+                client.onCloseWindow(window);
+            }
+        }
+
+        @Override
+        public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+            if(client != null) {
+                return client.onJsAlert(view, url, message, result);
+            }
+            return super.onJsAlert(view, url, message, result);
+        }
+
+        @Override
+        public boolean onJsConfirm(WebView view, String url, String message, JsResult result) {
+            if(client != null) {
+                return client.onJsConfirm(view, url, message, result);
+            }
+            return super.onJsConfirm(view, url, message, result);
+        }
+
+        @Override
         public boolean onJsPrompt(WebView view, String url, String message, String defaultValue, JsPromptResult result) {
             if (message != null && message.startsWith(JSINVOKER_PREFIX)) {
                 //invoke java method
@@ -80,6 +189,124 @@ public class SafeWebView extends WebView {
                 return true;
             }
             return client != null && client.onJsPrompt(view, url, message, defaultValue, result);
+        }
+
+        @Override
+        public boolean onJsBeforeUnload(WebView view, String url, String message, JsResult result) {
+            if(client != null) {
+                return client.onJsBeforeUnload(view, url, message, result);
+            }
+            return super.onJsBeforeUnload(view, url, message, result);
+        }
+
+        @Override
+        public void onExceededDatabaseQuota(String url, String databaseIdentifier, long quota, long estimatedDatabaseSize, long totalQuota, WebStorage.QuotaUpdater quotaUpdater) {
+            super.onExceededDatabaseQuota(url, databaseIdentifier, quota, estimatedDatabaseSize, totalQuota, quotaUpdater);
+            if(client != null) {
+                client.onExceededDatabaseQuota(url, databaseIdentifier, quota, estimatedDatabaseSize, totalQuota, quotaUpdater);
+            }
+        }
+
+        @Override
+        public void onReachedMaxAppCacheSize(long requiredStorage, long quota, WebStorage.QuotaUpdater quotaUpdater) {
+            super.onReachedMaxAppCacheSize(requiredStorage, quota, quotaUpdater);
+            if(client != null) {
+                client.onReachedMaxAppCacheSize(requiredStorage, quota, quotaUpdater);
+            }
+        }
+
+        @Override
+        public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+            super.onGeolocationPermissionsShowPrompt(origin, callback);
+            if(client != null) {
+                client.onGeolocationPermissionsShowPrompt(origin, callback);
+            }
+        }
+
+        @Override
+        public void onGeolocationPermissionsHidePrompt() {
+            super.onGeolocationPermissionsHidePrompt();
+            if(client != null) {
+                client.onGeolocationPermissionsHidePrompt();
+            }
+        }
+
+        @Override
+        public void onPermissionRequest(PermissionRequest request) {
+            super.onPermissionRequest(request);
+            if(client != null) {
+                if(Build.VERSION.SDK_INT >= 21) {
+                    client.onPermissionRequest(request);
+                }
+            }
+        }
+
+        @Override
+        public void onPermissionRequestCanceled(PermissionRequest request) {
+            super.onPermissionRequestCanceled(request);
+            if(client != null) {
+                if(Build.VERSION.SDK_INT >= 21) {
+                    client.onPermissionRequestCanceled(request);
+                }
+            }
+        }
+
+        @Override
+        public boolean onJsTimeout() {
+            if(client != null) {
+                return client.onJsTimeout();
+            }
+            return super.onJsTimeout();
+        }
+
+        @Override
+        public void onConsoleMessage(String message, int lineNumber, String sourceID) {
+            super.onConsoleMessage(message, lineNumber, sourceID);
+            if(client != null) {
+                client.onConsoleMessage(message, lineNumber, sourceID);
+            }
+        }
+
+        @Override
+        public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+            if(client != null) {
+                return client.onConsoleMessage(consoleMessage);
+            }
+            return super.onConsoleMessage(consoleMessage);
+        }
+
+        @Override
+        public Bitmap getDefaultVideoPoster() {
+            if(client != null) {
+                return client.getDefaultVideoPoster();
+            }
+            return super.getDefaultVideoPoster();
+        }
+
+        @Override
+        public View getVideoLoadingProgressView() {
+            if(client != null) {
+                return client.getVideoLoadingProgressView();
+            }
+            return super.getVideoLoadingProgressView();
+        }
+
+        @Override
+        public void getVisitedHistory(ValueCallback<String[]> callback) {
+            super.getVisitedHistory(callback);
+            if(client != null) {
+                client.getVisitedHistory(callback);
+            }
+        }
+
+        @Override
+        public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+            if(client != null) {
+                if(Build.VERSION.SDK_INT >= 21) {
+                    client.onShowFileChooser(webView, filePathCallback, fileChooserParams);
+                }
+            }
+            return super.onShowFileChooser(webView, filePathCallback, fileChooserParams);
         }
     }
 }
